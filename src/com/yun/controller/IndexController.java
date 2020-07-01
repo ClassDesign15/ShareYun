77d0c56b1b692d77a2422e5762ee4af45622c796
kilicmu <kilicmu3389@outlook.com>
2020-07-01 11:32:56 +0800

    项目模板提交

---
+++ src/com/yun/controller/IndexController.java
@@ -0,0 +1,25 @@
+package com.yun.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.yun.service.UserService;
+import com.yun.utils.UserUtils;
+
+@Controller
+public class IndexController {
+	@Autowired
+	private UserService userService;
+	
+	@RequestMapping("/index")
+	public String index(HttpServletRequest request) {
+		System.out.println("haha");
+		String username = UserUtils.getUsername(request);
+		String countSize = userService.getCountSize(username);
+		request.setAttribute("countSize", countSize);
+		return "index";
+	}
+}
